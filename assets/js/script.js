5bd101894f545cff9a94658ae9f6929cdfda490e
EMMATHEM-PC\EMMATHEM <faluatemitopeo@gmail.com>
2017-06-12 19:14:51 +0100

    tabs-added-stil working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
-$('#myFormTab a').click(function (e) {
+$('#myTabContent a').click(function (e) {
   e.preventDefault()
-  $(this).tab()
-})
+  $(this).tab('show')
+});
+
+
+// password reveal buttons
+    $('#spwd a').click(function (e) {
+        e.preventDefault();
+        $box = $($(this).closest('#spwd'));
+        $box.toggleClass('shown');
+        if ($box.hasClass('shown')) $('[type=password]', $box).attr('type', 'text');
+        else $('input', $box).attr('type', 'password');
+    });
